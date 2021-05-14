a0d0bd32238ff6e065b38fbbc7999b076f057e03
Joao Pedro <joaoplay16@gmail.com>
2021-05-13 22:52:35 -0300

    stable

--- main.py
+++ main.py
@@ -2,17 +2,6 @@ import os
 import shutil
 import re
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
 
 def accect_remove(text):
     text = re.sub(r'(?i)[ÁÀÂÃ]', 'a', text)
